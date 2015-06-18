aad313b27c78d030f387e18188b95d2365d4762b
hmendozap <heist.mendoza@gmail.com>
2015-06-18 14:50:47 +0200

    First Commit - Adding ICP and old vanes lib

---
+++ vanes_ICP_Accurate/vanes_ICP_Accurate.ino
@@ -0,0 +1,92 @@
+/*
+  Input Capture - Recipe 18.7 Arduino Cookbook
+*/
+
+const int inputCapturePin = 48; // Oder 49
+
+const int prescale = 8;
+const byte prescaleBits = B010; // Prescale Factor
+const long precision = (1000000/(F_CPU / 1000)) * prescale;
+
+const int numberOfEntries = 16;
+const int countSamplePeriod = 1000;
+
+volatile byte index = 0;
+volatile byte count_enabled = 0; // 0 disables - 1 Enables
+volatile unsigned int results[numberOfEntries]; // 16-bit value
+volatile int overflows = 0;
+
+unsigned long pulse_ON = 0;  // Time of enconder on
+unsigned long pulse_OFF = 0; // Time of enconder off
+
+unsigned long pulse_ON_avg = 0;  // Average time on
+unsigned long pulse_OFF_avg = 0; // Average time off
+
+const float encConversion = 359.91; // Rotary encoder conversion constant
+float angle = 0.0; // Should be float?
+unsigned long signalDuration = 0;
+
+/* ICR interrupt vector */
+ISR(TIMER5_CAPT_vect) {
+  TCNT5 = 0; // Reset the counter
+  if (count_enabled) {
+    if (index != 0 || bitRead(TCCR5B, ICES5) == true) { // Wait Raising Edge
+      if (index < numberOfEntries) {
+        results[index] = ICR5; // Save the InputCaptureRegister
+        index++;
+      }
+    }
+  }
+  TCCR5B ^= _BV(ICES5);  // togge bit to trigger on the other edge
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(inputCapturePin, INPUT);  // ICP pin as input
+
+  TCCR5A = 0; // Normal Counting Mode
+  TCCR5B = prescaleBits; // set prescale bits
+  TCCR5B |= _BV(ICES5); // enable input capture
+
+  bitSet(TIMSK5, ICIE5); // enable input capture interrupt for timer5
+  Serial.print("Pulses with precision: ");
+  Serial.println(precision); // report duration of each tick in nanoseconds
+}
+
+// This loops prints the numbe of pulses in the last second, showing min and max pulse widths
+void loop() {
+  count_enabled = 1; // enable sampling
+  delay(countSamplePeriod);
+  count_enabled = 0; // disable sampling
+  if (index > 0) {
+    //Serial.println("Duration in microseconds are: ");
+    for (byte i=0; i < numberOfEntries; i++) {
+      /*unsigned long duration;*/
+      /*duration = results[i] * precision; //pulse duration in nanoseconds*/
+      /*Serial.println(duration / 1000); // duration in microseconds*/
+      if (i % 2 == 0) {
+        pulse_OFF += results[i] * precision;
+      }
+      else {
+        pulse_ON += results[i] * precision;
+      }
+    }
+    pulse_ON_avg = pulse_ON / (500 * numberOfEntries);
+    pulse_OFF_avg = pulse_OFF / (500 * numberOfEntries);
+    signalDuration = (pulse_ON_avg + pulse_OFF_avg);
+    angle = ((pulse_ON_avg * signalDuration)/(pulse_ON_avg + pulse_OFF_avg)) - 1;
+    angle = (angle * encConversion) / signalDuration;
+    Serial.print("signal length: ");
+    Serial.println(signalDuration , DEC); // duration in microseconds
+    Serial.print("high length: ");
+    Serial.println(pulse_ON_avg , DEC); // duration in microseconds
+    Serial.print("low length: ");
+    Serial.println(pulse_OFF_avg , DEC); // duration in microseconds
+    Serial.print("angle: ");
+    Serial.println(angle , DEC); // duration in microseconds
+    index = 0;
+    pulse_OFF = 0;
+    pulse_ON = 0;
+  }
+}
---
+++ vanes_ISR_Cheapscience/vanes_ISR_Cheapscience.ino
@@ -0,0 +1,27 @@
+/*
+  Reading PWM from Interrupts
+  Code taken from cheapscience.com/2010/01/reading-servo-pwm-with-an-arduino.html
+*/
+volatile long pwm_val = 0;
+volatile long tmp_count = 0;
+
+#define inter0 (PINE & 0b00010000) // Faster than digital read ???
+
+void analyze() {
+  if (inter0)
+    tmp_count = micros(); // Positive edge
+  else
+    pwm_val = micros() - tmp_count; // Negative edge; get Pulse width
+}
+
+void setup() {
+  pinMode(2, INPUT);
+  Serial.begin(9600);
+  Serial.println("Ready for counting");
+  attachInterrupt(0, analyze, CHANGE);
+}
+
+void loop() {
+  delay(150);
+  Serial.println(pwm_val, DEC);
+}
